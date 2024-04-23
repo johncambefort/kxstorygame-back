c48a898ba2519e592d01c73cc8ad09f21e177c76
John Cambefort <john.cambefort@philips.com>
2024-04-23 19:26:08 -0400

    chore: fix setup

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # README
 
 A containerized ruby-on-rails postgresql backend for the kxstorygame app!
+
+# Instructions
+
+```sh
+docker compose up -d
+
+# create & seed the development db
+docker compose exec kxback rails db:setup RAILS_ENV='development'
+```
--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module KxstorygameBack
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.log_level = :debug
+    config.active_job.queue_adapter = :sidekiq
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
