14cc5379c9b9b88aed3a44f5f6c1fdcebd813a3a
John Cambefort <john.cambefort@philips.com>
2024-04-28 18:04:11 -0400

    feat: create users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_18_040838) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_28_215901) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_18_040838) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.text "contents"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
