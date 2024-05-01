70b1e0a2eae09bacb8aa2104c9baa5397bfce28c
John Cambefort <john.cambefort@philips.com>
2024-05-01 18:46:31 -0400

    feat: add friendly redirects

--- app/models/concerns/authentication.rb
+++ app/models/concerns/authentication.rb
@@ -9,6 +9,11 @@ module Authentication
     helper_method :user_signed_in?
   end
 
+  def authenticate_user!
+    store_location
+    redirect_to login_path, alert: "You need to login to access that page." unless user_signed_in?
+  end
+  
   def login(user)
     reset_session
     session[:current_user_id] = user.id
@@ -20,10 +25,6 @@ module Authentication
 
   def redirect_if_authenticated
     redirect_to root_path, alert: 'You are already logged in.' if user_signed_in?
-  end
-
-  def authenticate_user!
-    redirect_to login_path, alert: "You need to login to access that page." unless user_signed_in?
   end
 
   def forget(user)
@@ -48,5 +49,9 @@ module Authentication
 
   def user_signed_in?
     Current.user.present?
+  end
+
+  def store_location
+    session[:user_return_to] = request.original_url if request.get? && request.local?
   end
 end
