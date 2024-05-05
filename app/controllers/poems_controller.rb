02823ffcb19ecbb9e5395959dd05cd0e80f7f687
John Cambefort <john.cambefort@philips.com>
2024-05-05 19:49:46 -0400

    feat: show, edit, index poems

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
 class PoemsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
 
   def new
     @poem = Poem.new
   end
 
-  def edit; end
+  def index
+    @poems = Poem.all
+  end
+
+  def show
+    @poem = Poem.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
 
   def create
     @poem = Poem.new(poem_params)
@@ -16,6 +24,22 @@ class PoemsController < ApplicationController
       redirect_to root_path, notice: 'Poem successfully created'
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+  
+  def edit
+    @poem = Poem.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
+
+  def update
+    @poem = Poem.find_by_id(params[:id])
+    if @poem.update(poem_params)
+      @poem.users.push(current_user) if !@poem.users.include?(current_user)
+      redirect_to @poem
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
