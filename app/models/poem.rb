a186412c58d2899437a310fb6067ed3d128c0b1a
John Cambefort <john.cambefort@philips.com>
2024-05-06 19:16:15 -0400

    chore: add line breaks

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -9,4 +9,12 @@ class Poem < ApplicationRecord
     s = lines&.split
     s.nil? || s.length < 2 ? nil : "#{s.first} #{s.last}"
   end
+
+  def formatted_body
+    return nil if !title.present?
+
+    lines.split("\r\n").map do |line|
+      line.strip << "<br>"
+    end.join
+  end
 end
