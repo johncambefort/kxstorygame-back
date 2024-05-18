eb3d772a0f21e3ae1a9be7afe9a2cea2dfed6e41
John Cambefort <john.cambefort@gmail.com>
2024-05-17 23:32:44 -0400

    chore: create db when possible, remove render bin

--- entrypoints/docker-entrypoint.sh
+++ entrypoints/docker-entrypoint.sh
@@ -3,11 +3,12 @@
 set -o errexit
 
 bundle install
+
+bundle exec rails db:create # could fail, that's alright
+bundle exec rails db:migrate
+
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-
-bundle exec rails db:setup
-bundle exec rails db:migrate
 
 bundle exec rails s
 
